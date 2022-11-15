1971d61d34ff227093122ab53a6879d64709fe57
Christian Renold <653140+severus-sn4pe@users.noreply.github.com>
2022-11-15 14:59:39 +0100

    updated config

--- config/general.py
+++ config/general.py
@@ -4,5 +4,3 @@ TRAINED_MODEL_DIR = "trained_models"
 TENSORBOARD_LOG_DIR = "tensorboard_log"
 RESULTS_DIR = "results"
 
-# parameters for data sources
-BINANCE_BASE_URL = "https://data.binance.vision/"  # binance url
